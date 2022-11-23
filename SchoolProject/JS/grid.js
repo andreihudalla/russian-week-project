3541841c0efd364d73eb647b8885824a127882c3
Andrei Hudalla <46633092+andreihudalla@users.noreply.github.com>
2022-11-23 17:00:36 +0300

    Delete SchoolProject directory

--- SchoolProject/JS/grid.js
+++
@@ -1,7 +0,0 @@
-let grid_elements = document.querySelectorAll(".book-element")
-
-for (const elem of grid_elements) {
-    elem.addEventListener("click", function() {
-        window.location.href = "/book.html?bookName=" + elem.dataset.name + "&pages=" + elem.dataset.pages;
-    })
-}
--- SchoolProject/JS/loader.js
+++
@@ -1,4 +0,0 @@
-window.addEventListener("load", (event) => {
-    let LoadDiv = document.querySelector("#loader")
-    LoadDiv.setAttribute("class","hidden-loader")
-})
