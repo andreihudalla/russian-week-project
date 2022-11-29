3837e09138bfad007f983e003b25b1e96d5abf3a
Andrei Hudalla <46633092+andreihudalla@users.noreply.github.com>
2022-11-29 20:45:17 +0300

    Add files via upload

--- JS/grid.js
+++ JS/grid.js
@@ -5,3 +5,14 @@ for (const elem of grid_elements) {
         window.location.href = "/book.html?bookName=" + elem.dataset.name + "&pages=" + elem.dataset.pages;
     })
 }
+
+function Update() {
+    let backgound = document.querySelector("#back-image")
+    var body = document.body,
+    html = document.documentElement;
+    var height = Math.max( body.scrollHeight, body.offsetHeight, 
+    html.clientHeight, html.scrollHeight, html.offsetHeight );
+    backgound.setAttribute("style","height: " + height + "px; transition: height 0.4s;")
+};
+
+setInterval(Update, 100);
